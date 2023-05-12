629c809e881e914d41e3dc6bf4cc3dc144065e31
Daniil-SV <daniilsivacenko@gmail.com>
2023-05-13 01:33:05 +0300

    Minor changes

--- bindings/SupercellFlashNapi/objects/SWFTexture.cpp
+++ bindings/SupercellFlashNapi/objects/SWFTexture.cpp
@@ -1,6 +1,7 @@
 #include "SWFTexture.hpp"
 
-namespace scNapi {
+namespace scNapi
+{
     PROPERTY(
         SWFTexture,
         pixelFormat,
@@ -42,4 +43,59 @@ namespace scNapi {
         parent->height(ToNativeValue<uint16_t>(value)),
         return ToJSValue(info, parent->height())
     );
+
+    PROPERTY(
+        SWFTexture,
+        data,
+        parent->data = TypeConvertor<std::vector<uint8_t>>::ToNativeValue(value),
+        return Napi::Buffer<uint8_t>::Copy(info.Env(), parent->data.data(), parent->data.size())
+    );
+
+    Napi::Value SWFTexture::getLinearData(const Napi::CallbackInfo& info)
+    {
+        SWFTexture* texture = SWFTexture::Unwrap(info[0].ToObject());
+        bool toLinear = ToNativeValue<bool>(info[1]);
+
+        std::vector<uint8_t> buffer = sc::SWFTexture::getLinearData(*(texture->get_parent()), toLinear);
+
+        return Napi::Buffer<uint8_t>::Copy(info.Env(), buffer.data(), buffer.size());
+    }
+
+    Napi::Value SWFTexture::getPixelFormatData(const Napi::CallbackInfo& info)
+    {
+        std::vector<uint8_t> buffer;
+        if (info[0].IsBuffer())
+        {
+            std::vector<uint8_t> imageData = TypeConvertor<std::vector<uint8_t>>::ToNativeValue(info[0]);
+            uint16_t width = ToNativeValue<uint16_t>(info[1]);
+            uint16_t heigt = ToNativeValue<uint16_t>(info[2]);
+            uint8_t srcType = ToNativeValue<uint8_t>(info[3]);
+            uint8_t dstType = ToNativeValue<uint8_t>(info[4]);
+
+            buffer = sc::SWFTexture::getPixelFormatData(imageData.data(), width, heigt, (sc::SWFTexture::PixelFormat)srcType, (sc::SWFTexture::PixelFormat)dstType);
+        }
+        else
+        {
+            sc::SWFTexture* texture = scNapi::SWFTexture::Unwrap(
+                info[0].As<Napi::Object>()
+            )->get_parent();
+            uint8_t dstType = ToNativeValue<uint8_t>(info[1]);
+
+            buffer = sc::SWFTexture::getPixelFormatData(*texture, (sc::SWFTexture::PixelFormat)dstType);
+        }
+
+        return TypeConvertor<std::vector<uint8_t>>::ToJSBuffer(info, buffer);
+    }
+
+    Napi::Value SWFTexture::rescaleTexture(const Napi::CallbackInfo& info)
+    {
+        SWFTexture* texture = SWFTexture::Unwrap(info[0].ToObject());
+
+        uint16_t width = ToNativeValue<uint16_t>(info[1]);
+        uint16_t height = ToNativeValue<uint16_t>(info[2]);
+
+        std::vector<uint8_t> buffer = sc::SWFTexture::rescaleTexture(*(texture->get_parent()), width, height);
+
+        return Napi::Buffer<uint8_t>::Copy(info.Env(), buffer.data(), buffer.size());
+    }
 }
--- bindings/SupercellFlashNapi/objects/SWFTexture.hpp
+++ bindings/SupercellFlashNapi/objects/SWFTexture.hpp
@@ -63,62 +63,10 @@ namespace scNapi
 
         PROPERTY_DEF(width);
         PROPERTY_DEF(height);
+        PROPERTY_DEF(data);
     
-        static Napi::Value getLinearData(const Napi::CallbackInfo& info)
-        {
-            SWFTexture* texture = SWFTexture::Unwrap(info[0].ToObject());
-            bool toLinear = ToNativeValue<bool>(info[1]);
-
-            std::vector<uint8_t> buffer = sc::SWFTexture::getLinearData(*(texture->get_parent()), toLinear);
-
-            return Napi::Buffer<uint8_t>::Copy(info.Env(), buffer.data(), buffer.size());
-        }
-
-        static Napi::Value getPixelFormatData(const Napi::CallbackInfo& info)
-        {
-            std::vector<uint8_t> buffer;
-            if (info[0].IsBuffer())
-            {
-                std::vector<uint8_t> imageData = TypeConvertor<std::vector<uint8_t>>::ToNativeValue(info[0]);
-                uint16_t width = ToNativeValue<uint16_t>(info[1]);
-                uint16_t heigt = ToNativeValue<uint16_t>(info[2]);
-                uint8_t srcType = ToNativeValue<uint8_t>(info[3]);
-                uint8_t dstType = ToNativeValue<uint8_t>(info[4]);
-
-                buffer = sc::SWFTexture::getPixelFormatData(imageData.data(), width, heigt, (sc::SWFTexture::PixelFormat)srcType, (sc::SWFTexture::PixelFormat)dstType);
-            }
-            else
-            {
-                sc::SWFTexture* texture = scNapi::SWFTexture::Unwrap(
-                    info[0].As<Napi::Object>()
-                )->get_parent();
-                uint8_t dstType = ToNativeValue<uint8_t>(info[1]);
-
-                buffer = sc::SWFTexture::getPixelFormatData(*texture, (sc::SWFTexture::PixelFormat)dstType);
-            }
-
-            return TypeConvertor<std::vector<uint8_t>>::ToJSBuffer(info, buffer);
-        }
-
-        static Napi::Value rescaleTexture(const Napi::CallbackInfo& info)
-        {
-            SWFTexture* texture = SWFTexture::Unwrap(info[0].ToObject());
-            
-            uint16_t width = ToNativeValue<uint16_t>(info[1]);
-            uint16_t height = ToNativeValue<uint16_t>(info[2]);
-
-            std::vector<uint8_t> buffer = sc::SWFTexture::rescaleTexture(*(texture->get_parent()), width, height);
-
-            return Napi::Buffer<uint8_t>::Copy(info.Env(), buffer.data(), buffer.size());
-        }
-
-        void set_data(const Napi::CallbackInfo& info, const Napi::Value& value)
-        {
-            parent->data = TypeConvertor<std::vector<uint8_t>>::ToNativeValue(value);
-        }
-        Napi::Value get_data(const Napi::CallbackInfo& info)
-        {
-            return Napi::Buffer<uint8_t>::Copy(info.Env(), parent->data.data(), parent->data.size());
-        }
+        static Napi::Value getLinearData(const Napi::CallbackInfo&);
+        static Napi::Value getPixelFormatData(const Napi::CallbackInfo&);
+        static Napi::Value rescaleTexture(const Napi::CallbackInfo&);
     };
 }
